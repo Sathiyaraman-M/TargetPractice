0bde22cc81faa2c71223b4b9eacb8a8132258477
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-04-02 22:26:13 +0530

    Update binToDec.cpp

--- Day1/binToDec.cpp
+++ Day1/binToDec.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int binToDec(int n)
 {
-    int index = 0, result = 0, digit;
+    int mul = 1, result = 0, digit;
     while(n > 0)
     {
         digit = n % 10;
-        if (digit == 1)
-            result += pow(2, index);
-        index++;
+        result += mul * digit;
+        mul *= 2;
         n /= 10;
     }
     return result;
