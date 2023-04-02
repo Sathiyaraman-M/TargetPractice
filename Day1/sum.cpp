a445af3cd78878c79cc76d71af1e4377765ec6ae
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-04-02 21:52:08 +0530

    Add sum.cpp

---
+++ Day1/sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int sum(int n)
+{
+    int s = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        s += i;
+    }
+    return s;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sum(n) << endl;
+}
